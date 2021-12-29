a70100b29d34f4a744efbafa83e3e09988b8a2e4
noam.manyfler <noam.ma@moonactive.com>
2021-12-29 12:51:49 +0200

    Added main class

---
+++ src/main/java/com/manyf/Main.java
@@ -0,0 +1,9 @@
+package com.manyf;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
